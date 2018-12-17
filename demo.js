a689afdf84d12b78d5f95651bd2124cd126f92aa
liupo <liupo.wu@wz-inc.com>
2018-12-17 10:50:29 +0800

    add comment in demo

--- demo.js
+++ demo.js
@@ -6,6 +6,6 @@ var keywords = [ { text: '盘古大帝', priority: 10, max: 3 }, { text: '盘古
 var config = { replacement: '<a class="hot-keyword" href="#KEYWORD">KEYWORD</a>', placeholder: 'KEYWORD' };
 var kl = new KeywordLinker(keywords, config);
 
-var content = '<p data-tips="盘古大帝">宇宙混沌，鸿蒙初开，<a title="盘古大帝" href="#盘古大帝">盘古大帝劈开了天地</a>。</p><p class="盘古">盘古大帝、盘古大帝、盘古、盘古大帝、盘古、盘古大帝、盘古</p><p><img src="about:blank" alt="长相思、盘古、三皇五帝时期的故事" title="盘古大帝"></p>';
+var content = '<!--这是一段注释，来自长相思 盘古大帝 盘古--><p data-tips="盘古大帝">宇宙混沌，鸿蒙初开，<a title="盘古大帝" href="#盘古大帝">盘古大帝劈开了天地</a>。</p><p class="盘古">盘古大帝、盘古大帝、盘古、盘古大帝、盘古、盘古大帝、盘古</p><p><img src="about:blank" alt="长相思、盘古、三皇五帝时期的故事" title="盘古大帝"></p>';
 var newContent = kl.addLink2String(content); // Render for a string (替换字符串)
 console.log(newContent);
